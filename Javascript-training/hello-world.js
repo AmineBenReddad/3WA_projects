29a9c05fdc95cf74690898696364af8da55e87f4
AmineBenReddad <benreddadamine@gmail.com>
2023-05-02 10:41:10 +0100

    first Javascript training

---
+++ Javascript-training/hello-world.js
@@ -0,0 +1,4 @@
+console.log("Hello World !"),
+console.log(10 + 10);
+console.log("Peer")
+console.log('Hello, how are u ? :)')
---
+++ Javascript-training/variables.js
@@ -0,0 +1,29 @@
+var FirstName = "Amine"
+var age = 26
+var isMale = true;
+var balance = 100.33;
+var date = new Date(1997, 0, 22); // month 0 is January, 1 is February …
+var person = {};
+var empty = undefined;
+
+console.log(FirstName);
+console.log(age);
+console.log(isMale);
+console.log(balance);
+console.log(date);
+console.log(person);
+console.log(empty);
+
+console.log(typeof FirstName);
+
+
+
+
+/**    Naming Variables
+ *  Variables names :
+ *  - cannot contain spaces.
+ *  - must begin with a letter, an underscore (_) or a dollar sign ($)
+ *  - names can only contain letters, numbers, underscores, or dollar signs.
+ *  - names are case-sensitive.
+ *  - cannot use reserved keywords (const for example).
+ */
