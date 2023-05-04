6693d693783c221100f60c8b65cd9afc0dff78ef
AmineBenReddad <benreddadamine@gmail.com>
2023-05-04 11:22:59 +0100

    Capitalize the first letter

---
+++ Javascript-training/3wresource/capitalize.js
@@ -0,0 +1,11 @@
+function capitalize(str)
+{
+    if (str[0] != "")
+    {
+        let str2 = str.replace(str[0], str[0].toUpperCase())
+        console.log(str2);
+    }
+}
+
+capitalize("uhsdiuhoudhc")
+
