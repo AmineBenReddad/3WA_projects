d6eab55cceef5838909df3b8193f68b9107c32e4
AmineBenReddad <benreddadamine@gmail.com>
2023-05-09 11:00:23 +0100

    remove element from array

---
+++ Javascript-training/algo/removeElement.js
@@ -0,0 +1,11 @@
+const removeElement = (arr, elm) =>{
+    let arr2 = []
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        if (arr[i] != elm)
+        arr2.push(arr[i])
+    }
+    return arr2
+}
+
+console.log(removeElement([1,5,9,8,6,5,3,1], 1));
