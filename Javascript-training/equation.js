72150ee7b6b262342b2fd34eee58e37bbaa155b3
AmineBenReddad <benreddadamine@gmail.com>
2023-05-09 16:11:29 +0100

    equation first degree

---
+++ Javascript-training/equation.js
@@ -0,0 +1,15 @@
+let x
+// const e = (a, b ,c) => {
+//     if (a != 0)
+//     {
+//         return x = ((c - b) / a)
+//     }
+//     else
+//         return "error"
+// }
+
+const e = (a, b, c) => (a != 0) ? (c - b)/a : "error"
+
+
+console.log(e(2, 5, 3));
+
